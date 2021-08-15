063f576e0b75d15fa00f6e5254a5031e6d44b126
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-15 20:05:15 +0545

    Update username.js

--- commands/owners/username.js
+++ commands/owners/username.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'username',
   aliases: 'c-name',
 	execute: async(client, message, args) => {
-    if(message.author.id == "720632216236851260" || message.author.id == "780029934163722280") {
+    if(message.author.id == "852219497763045398") {
 		const embed = new Discord.MessageEmbed().setColor('#FF00FF')
 		await client.user.setUsername(args.join(' ')).then(
 			message.channel.send(embed.setDescription('You have successfully changed my username!'))
