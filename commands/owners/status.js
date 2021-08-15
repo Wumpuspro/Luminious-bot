82c932b28d34cc1f78348600492571eb62bfa6ea
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-15 20:03:28 +0545

    Update status.js

--- commands/owners/status.js
+++ commands/owners/status.js
@@ -6,7 +6,7 @@ module.exports = {
   name: "status",
   execute: async (client, message, args) => {
     
-  if(message.author.id == "720632216236851260" || message.author.id == "780029934163722280") { 
+  if(message.author.id == "852219497763045398") { 
     //ARGUMENT
     if(!args.length) {
       return message.lineReply("Please give status message")
