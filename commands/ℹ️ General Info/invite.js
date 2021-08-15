ab59e2e857adc2167d9c25c55cd36df82b0280ce
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-15 19:19:55 +0545

    Update invite.js

--- commands/ℹ️ General Info/invite.js
+++ commands/ℹ️ General Info/invite.js
@@ -9,10 +9,10 @@ try {
 const embed = new Discord.MessageEmbed()
   .setTitle("Invite the bot")
   .setColor('RANDOM')
-  .addField("Invite to Discord server", "[Invite the bot here (Recomended!)](https://www.dumb-dsc.tk/invite)\n[Invite the bot here (Admin Perm)](https://discord.com/oauth2/authorize?client_id=828959241586606110&scope=bot%20applications.commands&permissions=8)")
-  .addField("Server", `[Join to official server](https://www.dumb-dsc.tk/support)`)
+  .addField("Invite to Discord server", "[Invite the bot here (Recomended!)](https://www.dumb-dsc.tk/invite)\n[Invite the bot here (Admin Perm)](https://discord.com/oauth2/authorize?client_id=${clientid}&scope=bot%20applications.commands&permissions=8)")
+  .addField("Server", `[Join to official server](https://discord.gg/qScXBZnHe6)`)
   .setFooter("Bot created by Npg")
-  .setImage(`https://cdn.discordapp.com/attachments/789520293311676436/829675394616524869/standard_1.gif`)
+  .setImage(`https://images-ext-1.discordapp.net/external/eWe3DimWXdYRwGPHx_ai-sOKVNdS6YPc7s2ofj2Vbco/%3Fsize%3D1024/https/cdn.discordapp.com/icons/866236742739034120/a_5acf299e4ee75656b404686db3d1d2ff.gif`)
   .setTimestamp()
 message.author.send({embed})
 
